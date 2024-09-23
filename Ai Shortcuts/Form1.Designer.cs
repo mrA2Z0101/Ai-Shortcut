b8ab67848878043141160d54ecf61dc4ecda41ce
Aaron Zajicek <122756096+mrA2Z0101@users.noreply.github.com>
2024-09-23 00:32:08 -0500

    Add files via upload

---
+++ Ai Shortcuts/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace Ai_Shortcuts
+{
+    partial class Ai_Shortcuts
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openChatGPT = new System.Windows.Forms.Button();
+            this.openMidjourney = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openSunoAi = new System.Windows.Forms.Button();
+            this.openClaude = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFuturepedia = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.openGemini = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openChatGPT
+            // 
+            this.openChatGPT.Location = new System.Drawing.Point(-1, 42);
+            this.openChatGPT.Name = "openChatGPT";
+            this.openChatGPT.Size = new System.Drawing.Size(112, 34);
+            this.openChatGPT.TabIndex = 0;
+            this.openChatGPT.Text = "ChatGPT";
+            this.openChatGPT.UseVisualStyleBackColor = true;
+            this.openChatGPT.Click += new System.EventHandler(this.openChatGPT_Click);
+            // 
+            // openMidjourney
+            // 
+            this.openMidjourney.Location = new System.Drawing.Point(-1, 175);
+            this.openMidjourney.Name = "openMidjourney";
+            this.openMidjourney.Size = new System.Drawing.Size(112, 34);
+            this.openMidjourney.TabIndex = 1;
+            this.openMidjourney.Text = "Midjourney";
+            this.openMidjourney.UseVisualStyleBackColor = true;
+            this.openMidjourney.Click += new System.EventHandler(this.openMidjourney_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Chatbots";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-1, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Art Generators";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-1, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Music Generator";
+            // 
+            // openSunoAi
+            // 
+            this.openSunoAi.Location = new System.Drawing.Point(-1, 299);
+            this.openSunoAi.Name = "openSunoAi";
+            this.openSunoAi.Size = new System.Drawing.Size(112, 34);
+            this.openSunoAi.TabIndex = 5;
+            this.openSunoAi.Text = "Suno Ai";
+            this.openSunoAi.UseVisualStyleBackColor = true;
+            this.openSunoAi.Click += new System.EventHandler(this.openSunoAi_Click);
+            // 
+            // openClaude
+            // 
+            this.openClaude.Location = new System.Drawing.Point(117, 42);
+            this.openClaude.Name = "openClaude";
+            this.openClaude.Size = new System.Drawing.Size(112, 34);
+            this.openClaude.TabIndex = 6;
+            this.openClaude.Text = "Claude";
+            this.openClaude.UseVisualStyleBackColor = true;
+            this.openClaude.Click += new System.EventHandler(this.openClaude_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Perplexity";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFuturepedia
+            // 
+            this.openFuturepedia.Location = new System.Drawing.Point(-1, 413);
+            this.openFuturepedia.Name = "openFuturepedia";
+            this.openFuturepedia.Size = new System.Drawing.Size(127, 34);
+            this.openFuturepedia.TabIndex = 8;
+            this.openFuturepedia.Text = "Futurepedia";
+            this.openFuturepedia.UseVisualStyleBackColor = true;
+            this.openFuturepedia.Click += new System.EventHandler(this.openFuturepedia_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-1, 385);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ai Apps";
+            // 
+            // openGemini
+            // 
+            this.openGemini.Location = new System.Drawing.Point(353, 42);
+            this.openGemini.Name = "openGemini";
+            this.openGemini.Size = new System.Drawing.Size(112, 34);
+            this.openGemini.TabIndex = 10;
+            this.openGemini.Text = "Gemini";
+            this.openGemini.UseVisualStyleBackColor = true;
+            this.openGemini.Click += new System.EventHandler(this.openGemini_Click);
+            // 
+            // Ai_Shortcuts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 503);
+            this.Controls.Add(this.openGemini);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.openFuturepedia);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.openClaude);
+            this.Controls.Add(this.openSunoAi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.openMidjourney);
+            this.Controls.Add(this.openChatGPT);
+            this.Name = "Ai_Shortcuts";
+            this.Text = "Ai Shortcut";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openChatGPT;
+        private System.Windows.Forms.Button openMidjourney;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button openSunoAi;
+        private System.Windows.Forms.Button openClaude;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button openFuturepedia;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button openGemini;
+    }
+}
+
---
+++ Ai Shortcuts/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Ai_Shortcuts
+{
+    public partial class Ai_Shortcuts : Form
+    {
+        public Ai_Shortcuts()
+        {
+            InitializeComponent();
+        }
+
+        //Chatbots
+        private void openChatGPT_Click(object sender, EventArgs e)
+        {
+            string url = "https://chatgpt.com/"; // The URL you want to open
+            OpenUrl(url);
+        }
+
+        private void openGemini_Click(object sender, EventArgs e)
+        {
+            string url = "https://gemini.google.com/app"; // The URL you want to open
+            OpenUrl(url);
+        }
+
+        private void openClaude_Click(object sender, EventArgs e)
+        {
+            string url = "https://claude.ai/new"; // The URL you want to open
+            OpenUrl(url);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.perplexity.ai/"; // The URL you want to open
+            OpenUrl(url);
+        }
+
+        //Art Generators
+        private void openMidjourney_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.midjourney.com/explore?tab=top"; // The URL you want to open
+            OpenUrl(url);
+        }
+
+        //Music Generators
+        private void openSunoAi_Click(object sender, EventArgs e)
+        {
+            string url = "https://suno.com/"; // The URL you want to open
+            OpenUrl(url);
+        }
+
+        //Ai Apps
+        private void openFuturepedia_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.futurepedia.io/"; // The URL you want to open
+            OpenUrl(url);
+        }
+
+        // Method to open the URL
+        public void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
